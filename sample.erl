9fd21563e134ad04c92b92e5b8f56529ec298669
revdfdev <mkodekar@zoho.com>
2019-08-20 20:32:26 +0530

    Add a recurssive function

--- sample.erl
+++ sample.erl
@@ -10,8 +10,10 @@
   to_upper/1 
 ]).
 
--export([hello_world/0]).
+-export([hello_world/0, fac/1]).
 
+fac(N) when N == 0 -> 1;
+fac(N) when N > 0 -> N*fac(N-1).
 
 printLn(OUTPUT) ->
   io:fwrite(OUTPUT).
@@ -22,17 +24,14 @@ hello_world() ->
   X = 10,
   Y = 10,
   printLn(Greeting),
-  do_count(19),
   Result = X + Y,
   SubStra = X - Y,
   Mult = X * Y,
   Divi = X / Y,
+  XY = fac(9),
+  io:fwrite("~w~n", [XY]),
   io:fwrite(Greeting),
   io:fwrite("~w~n", [SubStra]),
   io:fwrite("~w~n", [Mult]),
   io:fwrite("~w~n", [Divi]),
   io:fwrite("~w~n", [Result]).
-
-
-do_count(0) -> 0;
-do_count(N) -> do_count(N - 1).
